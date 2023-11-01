35ff851757d9a233e1012972d3e41735ea5bc254
partial_ <anthony.r.andrews@gmail.com>
2023-10-31 19:14:10 -0700

    Update bridgeProject.rkt
    > (hand-error '(ha ha sa sk d4 d4 d2 ck cq cj c10 c9 c8)
    '(There are multiple aces of hearts)
    or
    > (hand-error '(ha ha)
    '(a valid hand has 13 cards).
    
    https://people.eecs.berkeley.edu/~bh/ssch9/bridge.html

--- bridgeProject.rkt
+++ bridgeProject.rkt
@@ -73,3 +73,87 @@
 (define (bridge-val bridge) ; calculate the total brdige hand valule based on distribution points and card points.
 (+ (hand-dist-points bridge) (high-card-points bridge) )
 )
+
+;(define (in-hand? card hand)
+;  (cond
+;  ((equal? (length hand) 1) (equal? (sentence card) hand))
+;  (else (sentence (equal? card (first hand) (in-hand? card (bf hand)))))
+;  )
+;)
+
+;(define (in-hand? card hand)
+;    (keep (lambda(x)(equal? card x)) hand)
+;)
+
+(define (duplicates? hand)
+    (if (equal? 1 (length hand)) 
+        #f
+        (or (member? (first hand) (bf hand)) (duplicates? (bf hand)))
+    )
+)
+
+(define (duplicates hand)
+    (if (equal? 1 (length hand)) 
+        #f
+        (if (or (member? (first hand) (bf hand)) (duplicates? (bf hand)))
+            (first hand)
+            'none ; this condition should never be reached under normal circumstances.
+        )
+    )
+)
+
+(define (is-card? card)
+    (if (member? (first card) '(c d h s)) 
+        (if (member? (bf card) '(a 2 3 4 5 6 7 8 9 10 j q k))
+            #t 
+            #f
+        )
+        #f
+    )
+)
+
+(define (card-suit card)
+    (cond
+        ((equal? (first card) 'c ) '(of clubs) )
+        ((equal? (first card) 'd ) '(of diamonds) )
+        ((equal? (first card) 'h ) '(of houses) )
+        ((equal? (first card) 's ) '(of spades) )
+    )
+)
+
+(define (card-name card)
+    (cond
+        ((equal? (bf card) 'a ) (sentence 'ace (card-suit card) ))
+        ((equal? (bf card) '2 ) (sentence '2 (card-suit card) ))
+        ((equal? (bf card) '3 ) (sentence '3 (card-suit card) ))
+        ((equal? (bf card) '4 ) (sentence '4 (card-suit card) ))
+        ((equal? (bf card) '5 ) (sentence '5 (card-suit card) ))
+        ((equal? (bf card) '6 ) (sentence '6 (card-suit card) ))
+        ((equal? (bf card) '7 ) (sentence '7 (card-suit card) ))
+        ((equal? (bf card) '8 ) (sentence '8 (card-suit card) ))
+        ((equal? (bf card) '9 ) (sentence '9 (card-suit card) ))
+        ((equal? (bf card) '10 ) (sentence '10 (card-suit card) ))
+        ((equal? (bf card) 'j ) (sentence 'jack (card-suit card) ))
+        ((equal? (bf card) 'q ) (sentence 'queen (card-suit card) ))
+        ((equal? (bf card) 'k ) (sentence 'king (card-suit card) ))
+    )
+)
+
+(define (invalid-card hand)
+    (if (is-card? (first hand))
+        (invalid-card (bf hand))
+        (first hand)
+    )
+)
+
+(define (hand-error hand)
+    (if (equal? (length (keep is-card? hand) )13 )
+        (if (duplicates? hand)
+            (sentence '(Invalid hand. There are more than one) (card-name (duplicates hand)))
+            '(Hand is valid.)
+        )
+        (if (equal? (length hand) 13 )
+            (sentence '(Invalid hand.) (invalid-card hand) '(is not a valid card.))
+            '(Invalid hand. A valid bridge hand has 13 cards.)
+    ))
+)
